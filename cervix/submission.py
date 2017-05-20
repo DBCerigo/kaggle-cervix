b28c8b12f8736f255d261bb426a34ece1d74ee51
Ali T <aliteeney@googlemail.com>
2017-05-20 10:38:48 +0100

    Move functions into modules

--- cervix/submission.py
+++ cervix/submission.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import math
 import pandas as pd
+import csv
 
 def _log_prob(p):
     p = max(min(p, 1 - 10**-15), 10**-15)
@@ -18,3 +19,13 @@ def compute_losses(sub_csv_path_or_sub_df):
     score = df.log_l.sum()/N
     df = df.sort_values('log_l', ascending=False)
     return score, df
+
+def write_submission_file(path, ids, preds):
+    """Write a submission file using the predictions returned from random forest
+    and ids returns from random_forest_transform"""
+    with open(path, 'w') as csv_f:
+        writer = csv.writer(csv_f)
+        writer.writerow(['image_name','Type_1','Type_2','Type_3'])
+        for i,row in enumerate(preds_probs):
+            path = [ids[i]+'.jpg']
+            writer.writerow(path+list(row))
