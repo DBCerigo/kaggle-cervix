28a8c2cd995053b5da37de103020a1ba112dcbf2
Ali T <aliteeney@googlemail.com>
2017-05-23 23:19:30 +0100

    Maybe fix plotting bug? Who knows

--- cervix/analysis.py
+++ cervix/analysis.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 
 def plot_log_errors(history):
     plt.plot(history.history['loss'])
-    if history.history['val_loss']:
+    if 'val_loss' in history.history:
         plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    #plt.legend(['train', 'test'], loc='upper left')
     plt.show()
     
