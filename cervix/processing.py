3931d33aaf05cee7e36a2c42336eae3ec2c1fccc
dbcerigo <dbcerigo@gmail.com>
2017-05-23 18:35:04 +0100

    Re-implement processing functions

--- cervix/processing.py
+++ cervix/processing.py
@@ -11,25 +11,35 @@ def save_img(img, path):
     cv2.imwrite(path,img)
     return path
     
-def transform_save_imgs(df, transform_func):
+def transform_save_imgs(df, transform_func, **args):
     for index, row in df.iterrows():
-        desc = transform_func.__name__
-        df.loc[index,desc+'_path'] = _transform_save_img(row,transform_func)
+        df.loc[index, 'processed_path'] = transform_func(row.path, **args)
     return df
 
-def _transform_save_img(row, transform_func):
-    path = row.path
-    return transform_func(path)
 
-def grayscale_resize(path):
-    desc = 'grayscale_resize'
+# =======================================================
+# BEGIN PROCESSING FUNCTIONS
 
+# helper
+def __make_processed_path(path, desc):
     filename = os.path.basename(path)
     directories = path.split('/')
     #subdir here is either 'train' or 'test'
     subdir = directories[2]
+    return '../data/processed/'+desc+'/'+subdir+'/'+filename
 
-    gray_path = "../data/processed/"+desc+'/'+subdir+"/"+filename
+# should follow format as:
+# EXAMPLE
+def __processing_func(path):
+    processed_unique_path =__make_processed_path(path, 'unique_desc')
+        #this defines the path where the images are saved locally
+    #loading and processing here
+    img_objct = None
+    return save_img(img_objct, processed_unique_path)
+
+
+def grayscale_resize(path):
+    gray_path = __make_processed_path(path, 'grayscale_resize')
     if os.path.exists(gray_path):
         return gray_path
     print(filename.split('.')[0], end='.')
@@ -38,24 +48,20 @@ def grayscale_resize(path):
     gray = cv2.cvtColor(rescaled, cv2.COLOR_RGB2GRAY).astype('float')
     return save_img(gray, gray_path)
 
-def resize_n(n):
-    return lambda x: __resize_n(x, n)
-
-
-def __resize_n(path, n):
+def resize(path, n=299):
     assert isinstance(n, (int)), 'n must be an int'
-    desc = 'resize_'+str(n) 
-    filename = os.path.basename(path)
-    directories = path.split('/')
-    #subdir here is either 'train' or 'test'
-    subdir = directories[2]
-    resize_path = "../data/processed/"+desc+'/'+subdir+"/"+filename
+    resize_path = __make_processed_path(path, 'resize_'+str(n))
     if os.path.exists(resize_path):
         return resize_path
     print(filename.split('.')[0], end='.')
     img = cv2.imread(path)
     rescaled = cv2.resize(img, (n, n), cv2.INTER_LINEAR)
     return save_img(rescaled, resize_path)
+
+# END processing functions
+# ===========================================
+
+
 
 def random_forest_transform(df, img_path_column, grayscale=None):
     assert isinstance(grayscale, (bool)), 'grayscale must be set to a bool'
@@ -64,15 +70,15 @@ def random_forest_transform(df, img_path_column, grayscale=None):
     df['vec'] = df[img_path_column].map(path_to_vec)
     return df
 
+def process_image(img):
+    '''Normalize image and turn into array of one long column'''
+    normalized = cv2.normalize(img, None, 0, 1, cv2.NORM_MINMAX)
+    vec = normalized.reshape(1, np.prod(normalized.shape))
+    return vec / np.linalg.norm(vec)
+
 def append_probabilities(orig_df, preds, type_order):
     type_columns = ['Type_'+t for t in type_order]
     index = orig_df.index.values
     probs_df = pd.DataFrame.from_records(preds, index=index, columns=type_columns)
     orig_df[type_columns] = probs_df[type_columns]
     return orig_df
-
-def process_image(img):
-    """Normalize image and turn into array of one long column"""
-    normalized = cv2.normalize(img, None, 0, 1, cv2.NORM_MINMAX)
-    vec = normalized.reshape(1, np.prod(normalized.shape))
-    return vec / np.linalg.norm(vec)
