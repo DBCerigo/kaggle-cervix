e315074c4cfbbf9cabd064339ed87aec7786bf48
Ali T <aliteeney@googlemail.com>
2017-05-26 15:07:50 +0100

    Add directory settings to train model shell wrapper

--- bin/train_model.sh
+++ bin/train_model.sh
@@ -1,5 +1,4 @@
 #PBS -N train_model
 #PBS -d ./
-echo pwd
-#source activate test_env
-#python /home/u3760/cervix/bin/train_model.py
+source activate test_env
+python /home/u3760/cervix/bin/train_model.py
