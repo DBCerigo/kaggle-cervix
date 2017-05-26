0d0b5db8f68103e313b9d5f43cefc4973705f9d7
Ali T <aliteeney@googlemail.com>
2017-05-26 15:02:25 +0100

    Testing working directories in colfax

--- bin/train_model.sh
+++ bin/train_model.sh
@@ -1,2 +1,5 @@
-source activate test_env
-python /home/u3760/cervix/bin/train_model.py
+#PBS -N train_model
+#PBS -d ./
+echo pwd
+#source activate test_env
+#python /home/u3760/cervix/bin/train_model.py
