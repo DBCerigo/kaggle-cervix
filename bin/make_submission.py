312f831d0e982e677b3485012f9898efdd892f13
Ali T <aliteeney@googlemail.com>
2017-06-10 20:04:42 +0100

    Fix qstat path bugs

--- bin/make_submission.py
+++ bin/make_submission.py
@@ -4,12 +4,12 @@ import sys
 import cv2
 import numpy as np
 from keras.models import load_model
-base_module_path = os.path.abspath(os.path.join('../cervix'))
+base_module_path = os.path.abspath(os.path.join('../'))
 if base_module_path not in sys.path:
     sys.path.append(base_module_path)
 import cervix as c
 
-model_path = '../model/v3_172_SGD_v3.h5'
+model_path = '/home/u3760/model/v3_172_SGD_v3.h5'
 model = load_model(model_path)
 test = c.data.make_test_df()
 test = c.processing.transform_save_imgs(test, c.processing.resize_n, n=299)
